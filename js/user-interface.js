27be9f0a2c4c62f080e5f38b41e23e71176c924f
eddie <e.jamesduro@gmail.com>
2016-03-18 10:00:07 -0700

    initial commit

--- js/user-interface.js
+++ js/user-interface.js
@@ -1,13 +1,5 @@
-// var displayMissingBike = require ('../js/bike.js').displayMissingBike;
-//
-// $(document).ready(function() {
-//   $('#missingBike').click(function() {
-//     //get city from user
-//     var city = $('#yourCity').val();
-//     //clear out city field
-//     $('#yourCity').val("");
-//     //run api call
-//     displayMissingBike(city);
-//     event.preventDefault();
-// });
-// });
+var user = require ('../js/user.js').user;
+
+$(document).ready(function() {
+
+});
