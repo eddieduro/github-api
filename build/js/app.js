197548cc125ad0706a458b21dd6d8cbfc99aba2f
eddieduro <e.jamesduro@gmail.com>
2016-03-23 17:41:10 -0700

    fix images

--- build/js/app.js
+++ build/js/app.js
@@ -7,7 +7,7 @@ var apiKey = require("./../.env").apiKey;
 exports.getRepos = function(user){
 	var url = "https://api.github.com/users/";
   	$.get(url + user + "?access_token=" + apiKey ).then(function(response){
-			console.log(response);
+
 		var name = response.name;
 		var image = response.avatar_url;
 		var profileUrl = response.html_url;
@@ -20,14 +20,14 @@ exports.getRepos = function(user){
 			var reposUrl = response.repos_url;
 			$.get(reposUrl + "?page=2").then(function(response2){
 				$.each(response2, function(i, repo){
-					console.log(i, repo);
+
 					$('.profilePic').html("<img id='avatar' src='"+ image +"'>");
 					$('#caption').text(name);
 					$('#profileLink').html("<p id='profileLink'><a href='"+ profileUrl +" 'target='_blank''>" + profileUrl + "</a></p>");
 					$('#result').html("<h3>" + name + "'s repositories</h3>");
 					$('#repos').removeClass('emptyList').prepend("<li class='animated slideInDown'><a target='_blank' href='"+ repo.svn_url +"'>" + repo.name + "</a><span> " + repo.description + "</span></li>");
 					if(hireable){
-						$('#hireable').html("<img src='../check.svg' id='checkmark'><span>Seeking Work</span>" + "<p id='location'>" + area +"</p>" );
+						$('#hireable').html("<img src='../../check.svg' id='checkmark'><span>Seeking Work</span>" + "<p id='location'>" + area +"</p>" );
 					} else {
 						$('#hireable').html("<p id='location'>" + area +"</p>" );
 					}
